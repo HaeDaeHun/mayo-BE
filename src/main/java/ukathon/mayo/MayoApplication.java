68b844d9ebaabfcd4b03b246b5bacc60e513a89d
judith <8w8u8@naver.com>
2025-07-29 17:41:59 +0900

    chore: 프로젝트 세팅

---
+++ src/main/java/ukathon/mayo/MayoApplication.java
@@ -0,0 +1,13 @@
+package ukathon.mayo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MayoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MayoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ukathon/mayo/MayoApplicationTests.java
@@ -0,0 +1,13 @@
+package ukathon.mayo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MayoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
