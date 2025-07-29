1f39e39279ab8935affa0f9641179d163e768df3
judith <8w8u8@naver.com>
2025-07-30 05:10:40 +0900

    fix: 시큐리티커스터마이징

--- src/main/java/ukathon/mayo/config/SecurityConfig.java
+++ src/main/java/ukathon/mayo/config/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig {
     public WebSecurityCustomizer webSecurityCustomizer() {
         return web -> web.ignoring()
                 .requestMatchers("/error", "/favicon.ico",
-                        "/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs", "/v3/api-docs/**"
+                        "/swagger-ui/**", "/swagger-ui.html", "/swagger-ui/index.html", "/v3/api-docs", "/v3/api-docs/**", "/webjars/**"
                 )
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
